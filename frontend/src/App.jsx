de3830cc43dc158ef5522b826311a1c58c497863
deepak <dgusaiwal537@gmail.com>
2024-01-08 21:16:08 +0530

    add frontent and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const App = () => {
+  const [data, setData] = useState("empty");
+
+  const fetchData = async () => {
+    try {
+      const {data} = await axios.get("/api/");
+      console.log('response is',data)
+      setData(data);
+    } catch (error) {
+      alert("failed to fetch data");
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>This is the Frontent File {data}</h1>
+    </div>
+  );
+};
+
+export default App;
