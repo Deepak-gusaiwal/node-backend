8d68e4bbcc627e1f41d6b81f13f007f7a2867c65
deepak <dgusaiwal537@gmail.com>
2024-01-10 21:04:46 +0530

    mongoose models

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 import express from "express";
-// import cors from "cors";
 const app = express();
-// app.use(cors());
-app.use(express.static("dist"));
+
 
 app.get("/api/", (req, res) => {
   res.send("Hello this is the hello World 2");
