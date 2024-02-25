15ebbeb71a6035459d69f6eac4898df33af466d8
deepak <dgusaiwal537@gmail.com>
2024-02-25 16:43:59 +0530

    second

--- backend/models/hospital/medicalrecord.model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordShcema = new mongoose.Schema({}, { timestamps: true });
-
-export const MedicalRecord = mongoose.model( "MedicalRecord", medicalRecordShcema);
