a6fa220010f1701e6493b0fc78d51a9eadc7d0c6
deepak <dgusaiwal537@gmail.com>
2024-03-04 22:00:14 +0530

    try to understand the asyncHandler function

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -12,4 +12,26 @@ const asyncHandler = (fn) => {
   };
 };
 
+
+// try to understand the above function
+
+//1. basic function {simply returning the function}
+// const fun1 = () => {
+//   return (a) => {
+//     console.log(a)
+//   };
+// };
+// const response = fun1();
+// response("hello")
+  
+// //2. return a callback function which can perform operations on passed value in original returned function
+// const fun1 = (fn) => {
+//   return (a) => {
+//     fn(a)
+//   };
+// };
+// const response = fun1((arg)=>{
+// console.log('hey',arg)
+// });
+// response("hello")
 export { asyncHandler };
